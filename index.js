f501f82605d27488a5fbe3a6bbba771a3db84fb6
prajapatiRgithub <prajapatiravi.shivinfotech@gmail.com>
2024-04-10 17:17:46 +0530

    added

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("Hello Ravin.");
-console.log("Rsviiiiiiiiiiiiiiiiiiiiiiiii");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
